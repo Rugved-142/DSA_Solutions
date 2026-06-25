f2d04d9922f21e8acd7deaf8c0f14d63852efb58
Rugved Ajayrao Gundawar <gundawar.r@northeastern.edu>
2026-06-25 17:20:18 -0400

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length == 0) return 0;
+        HashSet<Integer>  set = new HashSet<>();
+        int maxSize=1;
+        for(int n : nums){
+            set.add(n);
+        }
+        for(int n :  nums){
+            int length = 1;
+            if(!set.contains(n-1)){
+                while(set.contains(n+length)) length++;
+            }
+            maxSize = Math.max(maxSize,length);
+        }
+    return maxSize;
+    }
+}
