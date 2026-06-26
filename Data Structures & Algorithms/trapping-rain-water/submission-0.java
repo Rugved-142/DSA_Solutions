f475a67ecc4fd24cdd96803774331cb32339a2b9
Rugved Ajayrao Gundawar <gundawar.r@northeastern.edu>
2026-06-26 16:19:30 -0400

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] height) {
+        if(height == null || height.length == 0) return 0;
+
+        int left = 0;
+        int right = height.length-1;
+        int sum = 0;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        while( left < right){
+            if(leftMax < rightMax){
+                left ++;
+                leftMax = Math.max(leftMax , height[left]);
+                sum += leftMax - height[left];
+            }else{
+                right--;
+                rightMax = Math.max(rightMax,height[right]);
+
+                sum += rightMax - height[right];
+            }
+        }
+
+        return sum;
+    }
+}
