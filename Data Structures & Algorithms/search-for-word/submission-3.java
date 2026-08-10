524e498d3503eee9f8c5a064cf73828a18bc51b9
Rugved Ajayrao Gundawar <gundawar.r@northeastern.edu>
2026-08-10 11:52:55 -0400

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.java
@@ -0,0 +1,32 @@
+class Solution {
+        int ROW;
+        int COL;
+        private boolean[][] visited;
+    public boolean exist(char[][] board, String word) {
+        ROW = board.length;
+        COL = board[0].length;
+        visited = new boolean[ROW][COL];
+
+        for(int r = 0; r < ROW ; r++){
+            for(int c = 0; c < COL ; c++){
+                if(wordExist(board, word, r, c, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean wordExist (char[][] board, String word, int r, int c, int idx){
+        if(idx == word.length()) return true;
+
+        if(r < 0 || c < 0 || r >= ROW || c >= COL || board[r][c] != word.charAt(idx) || board[r][c] == '#') return false;
+
+        board[r][c] = '#';
+        boolean res = wordExist(board, word, r - 1, c, idx + 1) ||
+                      wordExist(board, word, r, c - 1, idx + 1) ||
+                      wordExist(board, word, r + 1, c, idx + 1) ||
+                      wordExist(board, word, r, c + 1, idx + 1);
+        board[r][c]=word.charAt(idx);
+
+        return res;
+    }
+}
