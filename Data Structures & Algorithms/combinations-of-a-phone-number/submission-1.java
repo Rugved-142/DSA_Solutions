319318e63494b33162289771bd27e0576a06548c
Rugved Ajayrao Gundawar <gundawar.r@northeastern.edu>
2026-08-10 18:27:43 -0400

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    List<String> res = new ArrayList<>();
+    private String[] digitToChar = {
+        "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
+    public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return res;
+        backtrack(0,"",digits);
+
+        return res;
+    }
+
+    private void backtrack(int i, String s, String digits){
+        if(s.length()==digits.length()){
+            res.add(s);
+            return;
+        }
+        
+        String chars = digitToChar[digits.charAt(i) - '0'];
+        for(char c : chars.toCharArray()){
+            backtrack(i+1, s + c,digits);
+        }
+    }
+}
