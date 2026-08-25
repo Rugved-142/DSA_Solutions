df280e32f7029746e7f828d96e9cd28c0963d6c1
Rugved Ajayrao Gundawar <gundawar.r@northeastern.edu>
2026-08-25 18:14:48 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer,List<Integer>> preMap = new HashMap<>();
+        for(int i = 0; i < numCourses; i++){
+            preMap.put(i,new ArrayList<>());
+        }
+        int[] indegree = new int[numCourses];
+
+        for(int[] prereq : prerequisites){
+            preMap.get(prereq[1]).add(prereq[0]);
+            indegree[prereq[0]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i]==0) q.add(i);
+        }
+
+        int finish = 0;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            finish++;
+            for(int nei : preMap.get(node)){
+                indegree[nei]--;
+                if(indegree[nei]==0) q.add(nei);
+            }
+        }
+        return finish == numCourses;
+    }
+}
